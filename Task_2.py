829b6cf7570ccb9bc8fbeaf4cb537dac3b0c680d
SAURAV RAUTHAN <sauravrauthan32@gmail.com>
2025-12-16 20:45:01 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,16 @@
+original_list=[1,2,3,4,5,6,7,8,9,10]
+
+Extracted_first_five_elements=original_list[0:5]
+
+
+
+reversed_extracted_elements=Extracted_first_five_elements[::-1]
+
+
+
+
+
+
+print(f"original_lists: {original_list}")
+print(f"Extracted_first_five_elements: {Extracted_first_five_elements}")
+print(f"reversed_extracted_elements: {reversed_extracted_elements}")
